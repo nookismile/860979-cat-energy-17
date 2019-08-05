8662e030361d2b0b4d7e2754cb7d1c8689333522
Cristina <stina.novickova@yandex.ru>
2019-08-06 00:02:33 +0300

    Без обновлений

---
+++ source/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+var navToggle = document.querySelector('.toggle-menu');
+var navMenu = document.querySelector('.main-nav');
+
+navMenu.classList.remove('main-nav--no-js');
+navToggle.classList.remove('toggle-menu--no-js');
+
+navToggle.addEventListener('click', function() {
+  navToggle.classList.toggle('toggle-menu--closed');
+  navMenu.classList.toggle('main-nav--opened');
+});
+
+
+
+
+
+
+
+
+
+
+
+});
